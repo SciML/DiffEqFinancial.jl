e7be302abdad1858c5134cd5a13eb9dbb6c32877
ChrisRackauckas <accounts@chrisrackauckas.com>
2025-07-31 05:21:29 -0400

    [ci-skip] Revert "Run JuliaFormatter.format()"
    This reverts commit 30c5b08.
    
    Reverting JuliaFormatter changes due to formatting issues being addressed in JuliaFormatter.jl PR#933.

--- docs/make.jl
+++ docs/make.jl
@@ -6,12 +6,12 @@ cp("./docs/Project.toml", "./docs/src/assets/Project.toml", force = true)
 include("pages.jl")
 
 makedocs(sitename = "DiffEqFinancial.jl",
-    authors = "Chris Rackauckas",
-    modules = [DiffEqFinancial],
-    clean = true,
-    doctest = false,
-    format = Documenter.HTML(assets = ["assets/favicon.ico"],
-        canonical = "https://docs.sciml.ai/DiffEqFinancial/stable/"),
-    pages = pages)
+         authors = "Chris Rackauckas",
+         modules = [DiffEqFinancial],
+         clean = true,
+         doctest = false,
+         format = Documenter.HTML(assets = ["assets/favicon.ico"],
+                                  canonical = "https://docs.sciml.ai/DiffEqFinancial/stable/"),
+         pages = pages)
 
 deploydocs(repo = "github.com/SciML/DiffEqFinancial.jl.git"; push_preview = true)
